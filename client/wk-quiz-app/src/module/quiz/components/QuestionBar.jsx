481cfd2e7b48471aad3f2cf36f1df94fac7ba873
JEDIWONG <jediwzk@gmail.com>
2025-09-01 17:16:10 +0800

    Feature : Added Quiz Timer To show Time Used For Answering Quiz

--- client/wk-quiz-app/src/module/quiz/components/QuestionBar.jsx
+++ client/wk-quiz-app/src/module/quiz/components/QuestionBar.jsx
@@ -1,4 +1,5 @@
 import '../style/QuestionBar.css';
+import QuizTimer from './QuizTimer';
 
 function QuestionBar(props){
     return(
@@ -6,6 +7,8 @@ function QuestionBar(props){
             <div className="question-bar-container">
                 <h1>Question {props.questionNo} <sub>of {props.total}</sub></h1>
                 <div className='question-bar-pts'>{props.pts} pts</div>
+
+                <QuizTimer /> 
             </div>
         </>
     )
