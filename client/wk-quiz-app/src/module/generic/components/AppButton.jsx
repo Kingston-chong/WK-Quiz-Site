7fcf51f9685eb02f9fd962dc7b9e5939521f5e02
JEDIWONG <jediwzk@gmail.com>
2025-09-04 22:07:17 +0800

    Enhance : Refactor code for rendering button

--- client/wk-quiz-app/src/module/generic/components/AppButton.jsx
+++ client/wk-quiz-app/src/module/generic/components/AppButton.jsx
@@ -1,14 +1,42 @@
 import { Button } from "@mui/material";
 import '../style/AppButton.css'
+import { Link } from "react-router-dom";
 
 /** Custom MUI Button 
  * params : type, title, url */
-function AppButton(props){
+
+function NavigationButton(props){
     return(
         <>
-            <Button className={`btn-${props.type}`} variant="contained" href={props.url}>{props.title}</Button>
+            <Link className="nav-button-link" to={props.url}>
+                <Button className={`btn-${props.type}`} variant="contained">{props.title}</Button>
+            </Link>
         </>
     )
+};
+
+
+function AppButton(props){
+
+    if(props.url!=null){
+        return(
+            <>
+                <NavigationButton 
+                    url = {props.url}
+                    type= {props.type}
+                    title = {props.title}
+                />  
+            </>
+        );
+    }
+    else{
+        return(
+            <>
+                <Button className={`btn-${props.type}`} variant="contained" href={props.url}>{props.title}</Button>
+            </>
+        );
+    }
+    
 }
 
 export default AppButton;
