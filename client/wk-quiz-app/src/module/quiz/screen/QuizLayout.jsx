433ed9a6f1abdf4b9b42f5bac1c29e98d7b837e9
JEDIWONG <jediwzk@gmail.com>
2025-09-13 12:52:05 +0800

    Enhance : Prompt On Getting User Confirmation When Refreshing Page

--- client/wk-quiz-app/src/module/quiz/screen/QuizLayout.jsx
+++ client/wk-quiz-app/src/module/quiz/screen/QuizLayout.jsx
@@ -1,7 +1,20 @@
 import { Outlet } from "react-router-dom";
 import '../style/QuizLayout.css';
+import { useEffect } from "react";
 
 function QuizLayout(){
+    useEffect(()=>{
+        const handleBeforeUnload = (e) => {
+            e.preventDefault();
+            e.returnValue = ""; 
+          };
+      
+          window.addEventListener("beforeunload", handleBeforeUnload);
+      
+          return () => {
+            window.removeEventListener("beforeunload", handleBeforeUnload);
+          };
+    },[])
     return(
         <>  
             <div className="quiz-layout-container">
